21d379c6767bc08be6e41233d4da0cd1bcd8037d
Irfan <irfanalamt@gmail.com>
2023-06-20 17:39:26 -0400

    Style: UI cleanup and index redesign

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import ApiIcon from '@mui/icons-material/Api';
 import MoodIcon from '@mui/icons-material/Mood';
-import { Button, Paper } from '@mui/material';
+import {Button, Card, CardContent, Paper} from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 import axios from 'axios';
-import { useState } from 'react';
+import {useState} from 'react';
 
 export default function Index() {
   const [newsData, setNewsData] = useState(null);
@@ -24,113 +24,98 @@ export default function Index() {
   }
 
   // set score bgColor based on sentiment score
-  function setBgColor(score) {
-    if (score < 0) return '#ec407a';
-
-    if (score >= 1) return '#9ccc65';
-
-    return '#29b6f6';
+  function setBgColor(article) {
+    if (article.vote === 'negative' || article.score < 0) return '#ef5350';
+    else return '#66bb6a';
   }
 
   function calculateSentiment() {
     // Extract desciption from articles; create newArray of article descriptions
-    const newsDescriptionOnly = newsData.map((el) => el.description);
-    console.log(
-      '🚀 ~ calculateSentiment ~ newsDescriptionOnly',
-      newsDescriptionOnly
+    const newsDescriptionAndTitle = newsData.map(
+      (el) => `${el.title}.${el.description}`
     );
 
     // Send array to backend; backend does sentiment analysis using NLP; return array of sentiment scores
     axios
       .post('/api/sentiment', {
-        newsArray: newsDescriptionOnly,
+        newsArray: newsDescriptionAndTitle,
       })
-      .then((res) => setSentimentData(res.data.sentiment))
+      .then((res) => {
+        setSentimentData(res.data.sentiment);
+        console.log('res = ' + JSON.stringify(res.data.sentiment[0]));
+      })
       .catch((err) => console.log('err:', err));
   }
 
   return (
-    <Container maxWidth='md'>
-      <Box sx={{ my: 2 }}>
-        <Typography
-          sx={{
-            backgroundColor: '#eceff1',
-            width: 'max-content',
-            p: 1,
-            boxShadow: 1,
-            m: 1,
-            fontSize: '3rem',
-          }}
-          variant='h3'
-          component='h3'
-          gutterBottom
-        >
-          Fetch news
-        </Typography>
+    <Container maxWidth='md' sx={{p: 2}}>
+      <Typography
+        variant='h4'
+        align='left'
+        sx={{fontWeight: 'bold', fontFamily: 'monospace', mb: 1}}>
+        WORD TRENDS
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: {xs: 'column', sm: 'row'},
+          mb: 4,
+          gap: 1,
+        }}>
         <Button
-          sx={{ m: 1, backgroundColor: '#26a69a', color: 'black' }}
+          variant='outlined'
           onClick={callNewsAPI}
-          variant='contained'
-          size='small'
-        >
+          startIcon={<ApiIcon />}>
           Call API
-          <ApiIcon sx={{ mx: 0.5, fontSize: '1.2rem' }} />
         </Button>
         <Button
-          sx={{ m: 1, backgroundColor: '#ffa726', color: 'black' }}
-          onClick={calculateSentiment}
           variant='contained'
-          size='small'
+          color='secondary'
+          onClick={calculateSentiment}
           disabled={!newsData}
-        >
-          Analyze sentiment
-          <MoodIcon sx={{ mx: 0.5, fontSize: '1.2rem' }} />
+          startIcon={<MoodIcon />}>
+          Analyze Sentiment
         </Button>
-        <Grid sx={{ my: 2 }} container spacing={2}>
-          {newsData?.map((article, i) => (
-            <Grid sm={12} md={6} key={i}>
-              <Paper sx={{ mx: 1, px: 2, py: 1, backgroundColor: '#c5cae9' }}>
-                <Typography
-                  sx={{ fontSize: '1rem', backgroundColor: '#e8eaf6', px: 0.5 }}
-                  variant='h6'
-                >
+      </Box>
+      <Grid container spacing={2} alignItems='stretch'>
+        {newsData?.map((article, i) => (
+          <Grid item xs={12} sm={6} key={i}>
+            <Card variant='outlined' sx={{backgroundColor: '#f5f5f5'}}>
+              <CardContent>
+                <Typography variant='h5' component='h2' gutterBottom>
                   {article.title}
                 </Typography>
                 {sentimentData && (
-                  <Typography
+                  <Box
                     sx={{
-                      mr: 'auto',
-                      mt: 1,
-                      backgroundColor: setBgColor(
-                        sentimentData[i].score.toFixed(2)
-                      ),
-                      width: 'max-content',
-                      px: 1,
-                      borderRadius: 1,
-                    }}
-                    variant='subtitle2'
-                  >
-                    {sentimentData[i].score.toFixed(2)}
-                  </Typography>
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'flex-end',
+                      mt: 2,
+                    }}>
+                    <Typography variant='caption'>Sentiment Score</Typography>
+                    <Typography
+                      variant='body1'
+                      sx={{
+                        backgroundColor: setBgColor(sentimentData[i]),
+                        px: 1,
+                        py: 0.5,
+                        ml: 1,
+                      }}>
+                      {sentimentData[i].score.toFixed(2)}
+                    </Typography>
+                  </Box>
                 )}
-              </Paper>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-
-      <Typography
-        sx={{
-          textAlign: 'center',
-          fontSize: '1rem',
-          display: newsData ? 'flex' : 'none',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-        variant='overline'
-      >
-        Life is good. ✨
-      </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      {newsData && (
+        <Typography variant='body2' align='center' sx={{my: 3}}>
+          Life is good. ✨
+        </Typography>
+      )}
     </Container>
   );
 }
